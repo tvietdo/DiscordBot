af7bd2370772038f467aceb9a459270ed8f83aef
Trung Do <t.viet.do@gmail.com>
2018-03-13 20:57:16 -0400

    Added server greeting

--- app.js
+++ app.js
@@ -65,6 +65,16 @@ client.on("message", (message) => {
     }
 });
 
+// Create an event listener for new guild members
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find('name', 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
+
 async function handleItem(name, channel, server) {
     let itemUrlPart = convertToUrlString(name);
     var url = config.wikiURL + itemUrlPart;
